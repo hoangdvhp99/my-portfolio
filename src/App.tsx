864fefca03fabb5846b9484492f7c72b39b0f413
Hoang Dang <hoangdv.hp99@gmail.com>
2024-02-29 00:09:11 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import Navbar from './sections/Navbar';
-import AboutSection from './sections/AboutSection';
-import EducationSection from './sections/EducationSection';
-import SkillsSection from './sections/SkillsSection';
-import ExperienceSection from './sections/ExperienceSection';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="container-fluid p-0">
-        <AboutSection />
-        <hr className="m-0" />
-        <ExperienceSection />
-        <hr className="m-0" />
-        <EducationSection />
-        <hr className="m-0" />
-        <SkillsSection />
-        <hr className="m-0" />
-      </div>
-    </>
+    <div>Home page</div>
   );
 }
 
